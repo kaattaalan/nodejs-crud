c0b5cd372c7119011b1cec96b76ae60d7daf56dc
Appu S Babu <appu.sbabu@inseinc.com>
2020-11-19 20:54:13 +0530

    Added jwt authentication

---
+++ controllers/user.controller.js
@@ -0,0 +1,30 @@
+const bcrypt = require('bcrypt'),
+    jwt = require('jsonwebtoken'),
+    userRepo = require('../repo/userRepo');
+
+module.exports = {
+    create: (req, res, next) => {
+
+        userRepo.create({ name: req.body.name, email: req.body.email, password: req.body.password }, function(err, result) {
+            if (err)
+                next(err);
+            else
+                res.json({ status: "success", message: "User added successfully!!!", data: { name: result.name, email: result.email } });
+
+        });
+    },
+    authenticate: function(req, res, next) {
+        userRepo.findOne({ email: req.body.email }, function(err, userInfo) {
+            if (err) {
+                next(err);
+            } else {
+                if (bcrypt.compareSync(req.body.password, userInfo.password)) {
+                    const token = jwt.sign({ id: userInfo._id }, req.app.get('secretKey'), { expiresIn: '1h' });
+                    res.json({ status: "success", message: "user found!!!", data: { user: userInfo, token: token } });
+                } else {
+                    res.json({ status: "error", message: "Invalid email/password!!!", data: null });
+                }
+            }
+        });
+    }
+}
--- server.js
+++ server.js
@@ -1,16 +1,35 @@
 const itemsRouter = require('./routes/itemRouter'),
+    userRouter = require('./routes/userRouter'),
     express = require('express'),
     morgan = require('morgan'),
-    app = express();
+    app = express(),
+    jwt = require('jsonwebtoken');;
 
 //morgan for logging
 app.use(morgan('dev'));
+app.set('secretKey', 'nodeRestApi');
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.use('/items', itemsRouter);
+app.use('/user', userRouter);
+
+//Secure Routers
+app.use('/items', validateUser, itemsRouter);
+
+function validateUser(req, res, next) {
+    jwt.verify(req.headers['x-access-token'], req.app.get('secretKey'), function(err, decoded) {
+        if (err) {
+            res.json({ status: "error", message: err.message, data: null });
+        } else {
+            // add user id to request
+            //req.body.userId = decoded.id;
+            next();
+        }
+    });
+
+}
 
 
 const port = 3001;
